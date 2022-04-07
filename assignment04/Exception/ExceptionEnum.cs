d7462fde8b81c0d2b35c57321ccc546341a12eb1
Lukas Ames <Lukas.Ames75@outlook.com>
2022-04-07 17:20:18 -0400

    enums

--- assignment04/Exception/ExceptionEnum.cs
+++ assignment04/Exception/ExceptionEnum.cs
@@ -2,5 +2,11 @@ namespace assignment04.Exception;
 
 public enum ExceptionEnum
 {
-    
+    ACCOUNT_DOES_NOT_EXIST,
+    CREDIT_LIMIT_HAS_BEEN_EXCEEDED,
+    NAME_NOT_ASSOCIATED_WITH_ACCOUNT,
+    NO_OVERDRAFT,
+    PASSWORD_INCORRECT,
+    USER_DOES_NOT_EXIST,
+    USER_NOT_LOGGED_IN
 }

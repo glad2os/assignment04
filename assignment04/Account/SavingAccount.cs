64c59b53e2d039537a20062974db1dc73e150d9c
TaeJunChang <taja0903@naver.com>
2022-04-08 05:21:03 +0900

    TaeJunChang

--- assignment04/Account/SavingAccount.cs
+++ assignment04/Account/SavingAccount.cs
@@ -1,14 +1,36 @@
-namespace assignment04.Account;
+namespace assignment04.Account 
+{ 
 
 public class SavingAccount : Account
 {
-    public void Withdraw(double p0, Person p6)
+    private static double COST_PER_TRANSACTION = 0.05;
+    private static double INTEREST_RATE = 0.015;
+    private const int MONTH = 12;
+
+    public SavingAccount(double balance = 0) : base("SV-", balance)
     {
-        throw new NotImplementedException();
+        Balance = balance;
     }
 
-    public void Deposit(double p0, Person p3)
+    public void Withdraw(double amount, Person person)
     {
-        throw new NotImplementedException();
+        if (!this.IsUser(person.Name)) throw new AccountException(AccountException.ExceptionEnum.NAME_NOT_ASSOCIATED_WITH_ACCOUNT);
+        if (!person.IsAuthenticated) throw new AccountException(AccountException.ExceptionEnum.USER_NOT_LOGGED_IN);
+        if (this.Balance < amount) throw new AccountException(AccountException.ExceptionEnum.CREDIT_LIMIT_HAS_BEEN_EXCEEDED);
+        base.Deposit(-amount, person);
+
     }
+
+    public void Deposit(double amount, Person person)
+    {
+        base.Deposit(amount, person);
+    }
+    public override void PrepareMonthlyStatement()
+    {
+        var serviceFee = this.transactions.Count * COST_PER_TRANSACTION;
+        var interest = this.LowestBalance * (INTEREST_RATE / MONTH);
+        this.Balance += interest - serviceFee;
+        this.transactions.Clear();
+    }
+ }
 }
